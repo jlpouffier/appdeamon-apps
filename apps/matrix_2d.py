728de4c4cd401f96823981bc424e81767d98442a
JLo <jean.loic.pouffier@gmail.com>
2023-11-16 11:55:07 +0100

    Removed a few event from Pulse

--- apps/matrix_2d.py
+++ apps/matrix_2d.py
@@ -3,17 +3,10 @@ import hassapi as hass
 class matrix_2d(hass.Hass):
     def initialize(self):
         self.listen_state(self.callback_state_change)
-        self.listen_event(self.callback_event, ["google_assistant_query", "alexa_smart_home"])
-        self.listen_event(self.callback_event, "hue_event", type = "initial_press" )
-        
     
     def callback_state_change(self, entity, attribute, old, new, kwargs):
         if self.is_in_scope(entity) and self.entities.binary_sensor.home_occupied.state == "on":
             #self.log(entity)
-            self.call_service("script/pulse_2d_led_matrix") 
-    
-    def callback_event(self, event_name, data, kwargs):
-        if self.entities.binary_sensor.home_occupied.state == "on":
             self.call_service("script/pulse_2d_led_matrix") 
 
     def is_in_scope(self, entity):
